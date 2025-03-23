257552ab28afa9a6ee0964f822be9bf15d316ad5
Yousef <yousef.l.t.aldawoud@protonmail.com>
2025-03-23 14:47:10 +1300

    Fix formats

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+vim.cmd("set nu rnu")
 
 local function get_visual_selections()
-	vim.cmd('noau normal! "vy"')
-	return vim.fn.getreg("v")
+    vim.cmd('noau normal! "vy"')
+    return vim.fn.getreg("v")
 end
 
 require("packer-plugins")
@@ -15,51 +15,61 @@ vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", {})
 
 vim.keymap.set("n", "ff", builtin.find_files, {})
 vim.keymap.set("n", "fg", builtin.live_grep, {})
-vim.keymap.set("v", "fg", function()
-	local text = get_visual_selections()
-	builtin.live_grep({ default_text = text })
-end, { noremap = true, silent = true })
+vim.keymap.set(
+    "v",
+    "fg",
+    function()
+        local text = get_visual_selections()
+        builtin.live_grep({default_text = text})
+    end,
+    {noremap = true, silent = true}
+)
 
 local escape_lua_pattern
 do
-	local matches = {
-		["^"] = "%^",
-		["$"] = "%$",
-		["("] = "%(",
-		[")"] = "%)",
-		["%"] = "%%",
-		["."] = "%.",
-		["["] = "%[",
-		["]"] = "%]",
-		["*"] = "%*",
-		["+"] = "%+",
-		["-"] = "%-",
-		["?"] = "%?",
-	}
+    local matches = {
+        ["^"] = "%^",
+        ["$"] = "%$",
+        ["("] = "%(",
+        [")"] = "%)",
+        ["%"] = "%%",
+        ["."] = "%.",
+        ["["] = "%[",
+        ["]"] = "%]",
+        ["*"] = "%*",
+        ["+"] = "%+",
+        ["-"] = "%-",
+        ["?"] = "%?"
+    }
 
-	escape_lua_pattern = function(s)
-		return (s:gsub(".", matches))
-	end
+    escape_lua_pattern = function(s)
+        return (s:gsub(".", matches))
+    end
 end
 
-vim.keymap.set("n", "<leader>cf", function()
-	local linenum = vim.api.nvim_win_get_cursor(0)[1]
-	local relative_filepath =
-		string.gsub(vim.api.nvim_buf_get_name(0), escape_lua_pattern(vim.api.nvim_command_output("pw")) .. "/", "")
-	vim.cmd('let @"="' .. relative_filepath .. ":" .. linenum .. '"')
-	vim.cmd('let @+="' .. relative_filepath .. ":" .. linenum .. '"')
-	vim.api.nvim_notify("Copied filename to clipboard", 0, {})
-end, { noremap = true, silent = true })
+vim.keymap.set(
+    "n",
+    "<leader>cf",
+    function()
+        local linenum = vim.api.nvim_win_get_cursor(0)[1]
+        local relative_filepath =
+            string.gsub(vim.api.nvim_buf_get_name(0), escape_lua_pattern(vim.api.nvim_command_output("pw")) .. "/", "")
+        vim.cmd('let @"="' .. relative_filepath .. ":" .. linenum .. '"')
+        vim.cmd('let @+="' .. relative_filepath .. ":" .. linenum .. '"')
+        vim.api.nvim_notify("Copied filename to clipboard", 0, {})
+    end,
+    {noremap = true, silent = true}
+)
 
 vim.keymap.set("n", "fb", builtin.buffers, {})
 vim.keymap.set("n", "fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find Symbols" })
-vim.keymap.set("n", "<leader>fi", "<cmd>AdvancedGitSearch<CR>", { desc = "AdvancedGitSearch" })
-vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
-vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word under Cursor" })
-vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
-vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
-vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {desc = "Find Symbols"})
+vim.keymap.set("n", "<leader>fi", "<cmd>AdvancedGitSearch<CR>", {desc = "AdvancedGitSearch"})
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {desc = "Find Old Files"})
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, {desc = "Find Word under Cursor"})
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, {desc = "Search Git Commits"})
+vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, {desc = "Search Git Commits for Buffer"})
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, {desc = "Find Keymaps"})
 vim.keymap.set("n", "<C-h>", vim.cmd.UndotreeToggle)
 
 vim.api.nvim_set_keymap("n", "<C-t>", ":tabnew <CR>", {})
@@ -70,7 +80,7 @@ vim.api.nvim_set_keymap("v", "c", '"+y', {})
 vim.api.nvim_set_keymap("n", "<leader>f", ":Neoformat | update <CR>", {})
 local colorScheme = "catppuccin-mocha"
 vim.cmd("colorscheme " .. colorScheme)
-vim.cmd([[
+vim.cmd [[
 	highlight Normal guibg=none
 	highlight NonText guibg=none
 	highlight Normal ctermbg=none
@@ -83,14 +93,14 @@ vim.cmd([[
 	highlight TelescopePromptBorder guibg=NONE
 	highlight TelescopeResultsNormal guibg=NONE
 	highlight TelescopeResultsBorder guibg=NONE
-]])
-local isTransparent = false
+]]
+local isTransparent = true
 
 local toggleTransparent = function()
-	if isTransparent then
-		vim.cmd("colorscheme " .. colorScheme)
-	else
-		vim.cmd([[
+    if isTransparent then
+        vim.cmd("colorscheme " .. colorScheme)
+    else
+        vim.cmd [[
 			highlight Normal guibg=none
 			highlight NonText guibg=none
 			highlight Normal ctermbg=none
@@ -103,13 +113,13 @@ local toggleTransparent = function()
 			highlight TelescopePromptBorder guibg=NONE
 			highlight TelescopeResultsNormal guibg=NONE
 			highlight TelescopeResultsBorder guibg=NONE
-		]])
-	end
-	isTransparent = not isTransparent
+		]]
+    end
+    isTransparent = not isTransparent
 end
 
 vim.api.nvim_create_user_command(
-	"ToggleTransparent", -- string
-	toggleTransparent,
-	{}
+    "ToggleTransparent", -- string
+    toggleTransparent,
+    {}
 )
--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -4,52 +4,54 @@
 
 vim.cmd.packadd("packer.nvim")
 
-return require("packer").startup(function(use)
-	use("gruvbox-community/gruvbox")
-	use({ "catppuccin/nvim", as = "catppuccin" })
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	use({ "sbdchd/neoformat" })
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+return require("packer").startup(
+    function(use)
+        use "gruvbox-community/gruvbox"
+        use {"catppuccin/nvim", as = "catppuccin"}
+        -- Packer can manage itself
+        use "wbthomason/packer.nvim"
+        use {"sbdchd/neoformat"}
+        use {
+            "nvim-telescope/telescope.nvim",
+            tag = "0.1.4",
+            -- or                            , branch = '0.1.x',
+            requires = {{"nvim-lua/plenary.nvim"}}
+        }
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
-	use("lewis6991/gitsigns.nvim")
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
+        use {
+            "VonHeikemen/lsp-zero.nvim",
+            branch = "v1.x",
+            requires = {
+                -- LSP Support
+                {"neovim/nvim-lspconfig"},
+                {"williamboman/mason.nvim"},
+                {"williamboman/mason-lspconfig.nvim"},
+                -- Autocompletion
+                {"hrsh7th/nvim-cmp"},
+                {"hrsh7th/cmp-buffer"},
+                {"hrsh7th/cmp-path"},
+                {"saadparwaiz1/cmp_luasnip"},
+                {"hrsh7th/cmp-nvim-lsp"},
+                {"hrsh7th/cmp-nvim-lua"},
+                -- Snippets
+                {"L3MON4D3/LuaSnip"},
+                {"rafamadriz/friendly-snippets"}
+            }
+        }
+        use "lewis6991/gitsigns.nvim"
+        use {
+            "nvim-lualine/lualine.nvim",
+            requires = {"nvim-tree/nvim-web-devicons", opt = true}
+        }
 
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-	})
-end)
+        use {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            requires = {
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+                "MunifTanjim/nui.nvim"
+            }
+        }
+    end
+)
